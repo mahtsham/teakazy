90e00e7f7af06f5a91fb211d79651fa8c3c585bd
Ahtsham <muhammadahtsham357@gmail.com>
2023-07-24 14:28:49 +0500

    add validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,4 @@
 class Article < ApplicationRecord
+    validates :title , presence: true, length: {minimum: 6, macximum: 100}
+    validates :description, presence: true, length: {minimum:6, macximum: 500}
 end
