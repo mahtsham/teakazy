6e53f455c599fd1ca07b0fb4b9f315802c1823ba
Ahtsham <muhammadahtsham357@gmail.com>
2023-07-30 03:16:12 +0500

    stuck on bootstrap import

--- Gemfile
+++ Gemfile
@@ -6,6 +6,10 @@ ruby "2.7.3"
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
 gem "rails", "~> 7.0.6"
 
+gem "sassc-rails"
+
+gem 'bootstrap'
+
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
@@ -78,3 +82,5 @@ group :test do
 end
 
 gem "cssbundling-rails", "~> 1.2"
+
+gem "jsbundling-rails", "~> 1.1"
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module AlphaBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.assets.css_compressor = :sass
+
+    config.assets.enabled = true
+    config.assets.version = '1.0'
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
